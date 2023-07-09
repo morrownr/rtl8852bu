7a132cd48076e4a3260883d21d149b126c4f0dc4
morrownr <morrownr@gmail.com>
2023-07-09 04:29:43 -0500

    testing arm support

--- platform/i386_pc.mk
+++ platform/i386_pc.mk
@@ -8,8 +8,13 @@ EXTRA_CFLAGS += -DCONFIG_RADIO_WORK
 ifeq ($(CONFIG_SDIO_HCI), y)
 EXTRA_CFLAGS += -DRTW_WKARD_SDIO_TX_USE_YIELD
 endif
-SUBARCH := $(shell uname -m | sed -e s/i.86/i386/)
+
+# ensure gcc is using the correct ARCH name
+#SUBARCH := $(shell uname -m | sed -e s/i.86/i386/)
+#SUBARCH := $(shell uname -m | sed -e "s/i.86/i386/; s/x86_64/i386/; s/armv.l/arm/; s/aarch64/arm64/;")
+SUBARCH := $(shell uname -m | sed -e "s/i.86/i386/; s/armv.l/arm/; s/aarch64/arm64/;")
 ARCH ?= $(SUBARCH)
+
 CROSS_COMPILE ?=
 KVER  := $(shell uname -r)
 KSRC := /lib/modules/$(KVER)/build
